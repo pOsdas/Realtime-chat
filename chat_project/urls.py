403d71cbe0a3ea3695bf90b7a17139b7ce58ec57
pOsdas <ratmir1099@mail.ru>
2025-11-03 00:06:25 +0300

    added urls

---
+++ chat_project/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.response import Response
+from chat_app.config import pydantic_settings
+from rest_framework.decorators import api_view
+from rest_framework.authtoken import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+API_PREFIX = pydantic_settings.api.prefix
+API_V1_PREFIX = pydantic_settings.api.v1.prefix
+
+
+@api_view(['GET'])
+def root_hello(request):
+    return Response({"message": "hello from our chat_app"})
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', root_hello, name='root'),
+    path(f"{API_PREFIX}{API_V1_PREFIX}/", include("chat_app.api.v1.users.urls")),
+    path(f"{API_PREFIX}{API_V1_PREFIX}/", include("chat_app.api.v1.chat.urls")),
+
+    # Маршруты для OpenAPI схемы и Swagger UI / Redoc
+    path(f'{API_PREFIX}{API_V1_PREFIX}/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(f'{API_PREFIX}{API_V1_PREFIX}/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path(f'{API_PREFIX}{API_V1_PREFIX}/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(f'{API_PREFIX}{API_V1_PREFIX}/auth/', views.obtain_auth_token),
+]
